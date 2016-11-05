4216639fe135dd49215e391325114f9c9e3423f3
Thor Sideburns <thorsten.sideb0ard@gmail.com>
2016-11-05 01:32:44 -0700

    stacky

--- shunty.c
+++ shunty.c
@@ -43,9 +43,9 @@ typedef struct token {
 bool isnum(char *ch);
 bool isvalid_char(char *ch);
 bool isvalid_pattern(char *pattern);
-int precedence(int op);
-int associativity(int op); // left or right
-int parse_bytebeat(char *pattern);
+int  precedence(int op);
+int  associativity(int op); // left or right
+int  parse_bytebeat(char *pattern);
 
 //////////////////////////////////////////////////////////////////////////////
 
@@ -63,8 +63,8 @@ bool isvalid_char(char *ch)
         return true;
     
     // else check for valid chars in a bitwise operation
-    static char acceptable[] = {'<', '>', '|', '^', '~', '(', ')',
-                                '+', '-', '/', '*', '&', '%'};
+    static char acceptable[] = {'<', '>', '|', '^', '~', '+',
+                                '-', '/', '*', '&', '%'};
 
     int acceptable_len = strlen(acceptable);
     for ( int i = 0; i < acceptable_len; i++) {
@@ -132,121 +132,130 @@ int parse_bytebeat(char *pattern)
 
     if (!isvalid_pattern(pattern)) {
         printf("Beat it ya val jerk - acceptables chars are 0-9,"
-                "<, >, |, ^, ~, (, ), +, -, /, *, &, %%\n");
+                "<, >, |, ^, ~, +, -, /, *, &, %%\n");
         return EXIT_FAILURE;
     }
 
-    Stack *numberz = calloc(1, sizeof(List));
-    stack_init(numberz, NULL);
+    Stack *outputz = calloc(1, sizeof(List));
+    stack_init(outputz, NULL);
 
     Stack *operatorz = calloc(1, sizeof(List));
     stack_init(operatorz, NULL);
 
-    int pattern_len = strlen(pattern);
-    for (int i = 0; i < pattern_len; i++) {
+    // int pattern_len = strlen(pattern);
 
-        char *ch = &pattern[i];
-
-        // ignore space or EOF
-        if (*ch == 32 || *ch == 0)
-            continue;
-
+    char *sep = " ";
+    char *wurd, *wurdleft;
+    for (wurd = strtok_r(pattern, sep, &wurdleft);
+         wurd;
+         wurd = strtok_r(NULL, sep, &wurdleft))
+    {
         token *toke = calloc(1, sizeof(token));
-
-        if ( isnum(ch)) {
-            int pushy_num = atoi(ch);
+        printf("\nToken is %s\n", wurd);
+        printf("ATOI result is %d\n", atoi(wurd));
+        int val = atoi(wurd);
+        if (val != 0) {
+            printf("NUM! %d\n", val);
             toke->type = NUMBER;
-            toke->val = pushy_num;
-
-            stack_push(numberz, (void **)(toke));
-
-            int num_len = (int) log10(pushy_num) +1;
-            i += num_len;
+            toke->val = val;
+            stack_push(outputz, (void **)(toke));
         }
         else {
-            toke->type = OPERATOR;
-            switch (*ch) {
-            case(60):
-                toke->val = LEFTSHIFT;
-                i++;
-                break;
-            case(62):
-                toke->val = RIGHTSHIFT;
-                i++;
-                break;
-            case(94):
-                toke->val = XOR;
-                break;
-            case(124):
-                toke->val = OR;
-                break;
-            case(126):
-                toke->val = NOT;
-                break;
-            case(38):
-                toke->val = AND;
-                break;
-            case(43):
-                toke->val = PLUS;
-                break;
-            case(45):
-                toke->val = MINUS;
-                break;
-            case(42):
-                toke->val = MULTIPLY;
-                break;
-            case(47):
-                toke->val = DIVIDE;
-                break;
-            case(37):
-                toke->val = MODULO;
-                break;
-            case(40):
-                toke->val = LEFTBRACKET;
-                break;
-            case(41):
-                toke->val = RIGHTBRACKET;
-                break;
-            default:
-                printf("OOFT!\n");
-                return 1;
-            }
-            printf("OPS! %s\n", ops[toke->val]);
-            printf("OPS SIZE! %d\n", stack_size(operatorz));
-            // TODO - here - do the right thing
-            // if (stack_size(operatorz) > 0) {
-            //     bool wurk_it = true;
-            //     while(wurk_it) {
-            //         // check precedence
-            //         token *top_o_the_stack = stack_peek(operatorz);
-            //         if ((associativity(toke->val) == LEFT && precedence(toke->val) <= precedence(top_o_the_stack->val)) ||
-            //            (associativity(toke->val) == RIGHT && precedence(toke->val) < precedence(top_o_the_stack->val))) {
-            //                if (stack_pop(operatorz, (void **) &top_o_the_stack) == 0) {
-            //                    stack_push(numberz, (void **)top_o_the_stack);
-            //                } else printf("couldnae pop, capn\n");
-            //         } else {
-            //             wurk_it = false;
-            //         }
-            //     }
-            // }
-            stack_push(operatorz, (void **)(toke));
+            printf("OP! %s\n", wurd);
+
+             toke->type = OPERATOR;
+             if (strcmp(wurd, "<<") == 0)
+                 toke->val = LEFTSHIFT;
+             else if (strcmp(wurd, ">>") == 0)
+                 toke->val = RIGHTSHIFT;
+             else if (strcmp(wurd, "^") == 0)
+                 toke->val = XOR;
+             else if (strcmp(wurd, "|") == 0)
+                 toke->val = OR;
+             else if (strcmp(wurd, "~") == 0)
+                 toke->val = NOT;
+             else if (strcmp(wurd, "&") == 0)
+                 toke->val = AND;
+             else if (strcmp(wurd, "+") == 0)
+                 toke->val = PLUS;
+             else if (strcmp(wurd, "-") == 0)
+                 toke->val = MINUS;
+             else if (strcmp(wurd, "*") == 0)
+                 toke->val = MULTIPLY;
+             else if (strcmp(wurd, "/") == 0)
+                 toke->val = DIVIDE;
+             else if (strcmp(wurd, "%") == 0)
+                 toke->val = MODULO;
+             else {
+                 printf("OOFT!\n");
+                 return 1;
+             }
+             if (stack_size(operatorz) > 0) {
+                 printf("STACK SIZE GRETR THAN 0: %d\n", stack_size(operatorz));
+                 bool wurk_it = true;
+                 while(wurk_it && stack_size(operatorz) > 0) {
+                     token *top_o_the_stack = stack_peek(operatorz);
+                     printf("PRECEDENCE OF ME %d, precedence of top of stack %d\n", precedence(toke->val), precedence(top_o_the_stack->val));
+                     printf("ASSOCIATIVITY OF ME %s, ASSOCIATIVITY of top of stack %s\n",
+                             associativity(toke->val) ? "RIGHT" : "LEFT",
+                             associativity(top_o_the_stack->val) ? "RIGHT" : "LEFT");
+
+                     if (
+                        ((associativity(toke->val) == LEFT) &&
+                         (precedence(toke->val) <= precedence(top_o_the_stack->val)))
+                        ||
+                         ((associativity(toke->val) == RIGHT) && 
+                          (precedence(toke->val) < precedence(top_o_the_stack->val)))
+                        )
+                     {
+                        //    printf("GIN IN\n");
+                        if (stack_pop(operatorz, (void **) &top_o_the_stack) == 0) {
+                            stack_push(outputz, (void **) top_o_the_stack);
+                        } else {
+                            printf("Couldnae pop, mate\n");
+                        }
+                     } else {
+                            wurk_it = false;
+                            printf("WURKIT False\n");
+                     }
+                 }
+             }
+             stack_push(operatorz, (void **)(toke));
         }
     }
 
+    printf("\n\n======================================\n");
     token *re_token;
 
-    printf("NUMBERZ SIZE %d\n", stack_size(numberz));
-    while (stack_pop(numberz, (void **) &re_token) == 0) {
-      printf("Num %d\n", re_token->val);
+    printf("\nPOOOP\n");
+    while (stack_pop(operatorz, (void **) &re_token) == 0) {
+      printf("POP TYPE %d\n", re_token->type);
+      printf("POP val %d\n", re_token->val);
+      stack_push(outputz, (void**) re_token);
     }
 
-    printf("\nOPERATORZ SIZE %d\n", stack_size(operatorz));
+    printf("\nOUTPUTZ SIZE %d\n", stack_size(outputz));
+    while (stack_pop(outputz, (void **) &re_token) == 0) {
+      if ( re_token->type == NUMBER)
+        printf("Num %d\n", re_token->val);
+      else if ( re_token->type == OPERATOR)
+        printf("Op %s\n", ops[re_token->val]);
+      else
+        printf("Wowo, nelly!\n");
+    }
+
+    printf("\nOOPS SIZE %d\n", stack_size(operatorz));
     while (stack_pop(operatorz, (void **) &re_token) == 0) {
-      printf("OP %s\n", ops[re_token->val]);
+      if ( re_token->type == NUMBER)
+        printf("Num %d\n", re_token->val);
+      else if ( re_token->type == OPERATOR)
+        printf("Op %s\n", ops[re_token->val]);
+      else
+        printf("Wowo, nelly!\n");
     }
 
     stack_destroy(operatorz);
-    stack_destroy(numberz);
+    stack_destroy(outputz);
 
     return EXIT_SUCCESS;
 }
